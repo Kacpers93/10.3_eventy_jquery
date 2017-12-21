abe07d2cfbeb9c7fd4ace41c4ede5d57c21d5cbb
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-22 00:42:32 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@ $(function() {
 
     var carouselList = $("#carousel ul");
     function moveFirstSlide() {
-            var firstItem = carouselList.find("li:first"),
-                lastItem = carouselList.find("li:last");
-            lastItem.after(firstItem);
-            carouselList.css({marginLeft: 0});
-        };
+        var firstItem = carouselList.find("li:first"),
+            lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+        }
 
     function changeSlide (){
         carouselList.animate({'marginLeft':-400}, 1000, moveFirstSlide);
-    };
+    }
 
     setInterval(changeSlide, 3000);
 });
