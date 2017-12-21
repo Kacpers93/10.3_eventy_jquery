04d9aee9495aace0bb9640910951b94f01b61939
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-21 01:44:51 +0100

    Add all needed elements

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+    
+    var carouselList = $("#carousel ul"),
+        moveFirstSlide = function(){
+            var firstItem = carouselList.find("li:first"),
+                lastItem = carouselList.find("li:last");
+                lastItem.after(firstItem);
+                carouselList.css({marginLeft: 0});
+        };
+    
+    var changeSlide = function(){
+        carouselList.animate({'marginLeft':-400}, 1000, moveFirstSlide);
+    };
+    
+    setInterval(changeSlide, 3000);
+});
